d749941d7fa259721a248d0759469372d9addecb
Kevin McDonough <kmcdonough@placester.com>
2017-04-01 13:13:49 -0400

    Now has both endpoints

--- website/api.js
+++ website/api.js
@@ -5,8 +5,8 @@ let express = require('express');
 let router = express.Router();
 
 var lastMove = {
-  "dir": "stop",
-  "speed": 0
+  dir: "stop",
+  speed: 0
 }
 
 var resetAt = Date()
@@ -16,23 +16,25 @@ router.get('/move', (req, res) => {
   let dir = req.query.dir
   let speed = req.query.speed
   res.send('You moved ' + speed + ' meters ' + dir);
+  lastMove = {
+    dir: dir,
+    speed: speed
+  }
   reset = Date(Date.getTime() + resetAfterSec * 1000);
 
   // Autoreset
   SetTimeout(() => {
     if (Date() > resetAt) {
       lastMove = {
-        "dir": "stop",
-        "speed": 0
+        dir: "stop",
+        speed: 0
       }
     }
   }, resetAfterSec * 1000)
 });
 
 router.get('/getmove', (req, res) => {
-  res.json({
-    "dir":
-  })
+  res.json(lastMove);
 })
 
 
