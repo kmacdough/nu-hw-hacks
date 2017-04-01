cc6569c51bf9d610142b5665c0438b498ff92eee
Maxton <maxton@maxton.xyz>
2017-04-01 20:50:33 +0000

    change ssl

--- website/server.js
+++ website/server.js
@@ -17,14 +17,15 @@ app.get('/', function (req, res) {
 
 try {
   var sslOpts = {
-    key: fs.readFileSync('/etc/letsencrypt/live/hacks.maxton.xyz/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/hacks.maxton.xyz/fullchain.pem')
+    key: fs.readFileSync('ssl/privkey.pem'),
+    cert: fs.readFileSync('ssl/fullchain.pem')
   }
 
   https.createServer(sslOpts, app).listen(3443, function() {
     console.log("HTTPS server listening on port 3443")
   })
 } catch(ex) {
+  console.log("Error with HTTPS: "+ex);
   app.listen(3001, function () {
     console.log('HTTP server listening on port 3001!')
   })
