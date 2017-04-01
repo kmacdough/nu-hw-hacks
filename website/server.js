d010ccf5e18c3cbe1e5fa7e1487c4113229b271d
maxton <maxton@maxton.xyz>
2017-04-01 14:21:01 -0400

    adding static fileserving

--- website/server.js
+++ website/server.js
@@ -4,7 +4,7 @@ let express = require('express')
 let app = express()
 
 app.use('/api/v1', require('./api'))
-// app.use('/', requireg('./webapp'))
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
