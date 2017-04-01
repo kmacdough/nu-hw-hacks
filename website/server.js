3ec850f430179155a93ee78c521ca64243740c8b
maxton <maxton@maxton.xyz>
2017-04-01 16:40:43 -0400

    ssl test

--- website/server.js
+++ website/server.js
@@ -3,6 +3,9 @@
 let express = require('express')
 let app = express()
 
+let https = require('https')
+let fs = require('fs')
+
 app.use('/api/v1', require('./api'))
 app.use(express.static('public'))
 
@@ -10,6 +13,19 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
-})
+
+
+try {
+  var sslOpts = {
+    key: fs.readFileSync('/etc/letsencrypt/live/hacks.maxton.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/hacks.maxton.xyz/fullchain.pem')
+  }
+
+  https.createServer(sslOptions, app).listen(3443, function() {
+    console.log("HTTPS server listening on port 3443")
+  })
+} catch(ex) {
+  app.listen(3001, function () {
+    console.log('HTTP server listening on port 3001!')
+  })
+}
