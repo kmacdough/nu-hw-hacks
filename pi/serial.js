ab630270eee91a665874bdbcb39d04d1b28638ce
maxton <maxton@maxton.xyz>
2017-04-02 16:21:21 +0000

    fix serial

--- pi/serial.js
+++ pi/serial.js
@@ -1,26 +1,28 @@
 'use strict'
-
+let https = require('https')
 let SerialPort = require('serialport')
 
-var serialPort = new SerialPort("/dev/ttyACM0", {
+var serialPort = new SerialPort("/dev/ttyS0", {
     baudrate: 9600
 });
 
 serialPort.on("open", function () {
     console.log('open');
     while(true) {
-        http.get("https://hacks.maxton.xyz:3443/api/v1/controller", (res) => {
+        https.get("https://hacks.maxton.xyz:3443/api/v1/controller", (res) => {
+	    console.log("sending");
             res.setEncoding('utf8');
             let rawData = '';
             res.on('data', (chunk) => rawData += chunk);
             res.on('end', () => {
                 try {
+                    console.log("got data");
                     let parsedData = JSON.parse(rawData);
                     var b = new Buffer(4);
-                    b.writeInt8(parsedData.left[0] * 127, 0);
-                    b.writeInt8(parsedData.left[1] * 127, 1);
-                    b.writeInt8(parsedData.right[0] * 127, 2);
-                    b.writeInt8(parsedData.right[1] * 127, 3);
+                    b.writeInt8(parsedData.left[0] * 127 + 127, 0);
+                    b.writeInt8(parsedData.left[1] * 127 + 127, 1);
+                    b.writeInt8(parsedData.right[0] * 127 + 127, 2);
+                    b.writeInt8(parsedData.right[1] * 127 + 127, 3);
                     serialPort.write(b);
                 } catch (e) {
                     console.log(e.message);
